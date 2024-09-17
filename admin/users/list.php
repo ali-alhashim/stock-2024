bd5278199bd44f64f452ed8d52074adc1f87ea62
=Ali Alhashim <ali-alhashim@outlook.com>
2024-09-17 23:45:05 +0300

    add users

---
+++ admin/users/list.php
@@ -0,0 +1,58 @@
+<?php 
+session_start();
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Users Page</title>
+    <link href="../../static/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+  </head>
+    <body>
+
+    <?php
+      require("../base/main_menu.php");
+    ?>
+        <div class="container   my-1">
+
+             
+              <a href="add.php" class="btn btn-success my-5">Add</a>
+            
+            <table class="table table-bordered">
+                <thead>
+                    <th>#</th>
+                    <th>username</th>
+                    <th>role</th>
+                    <th><input type="checkbox" class="form-check-input" value="all"/></th>
+                </thead>
+                <tbody>
+                  <?php
+                    include '../base/config.php';
+                    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+                    $stmt = $conn->prepare("SELECT id, username, role  FROM users");
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    while ($row = $result->fetch_assoc()) 
+                    {     
+                      echo '<tr>
+                                 <td>'.$row["id"].'</td>
+                                 <td>'.$row["username"].'</td>
+                                 <td>'.$row["role"].'</td>
+                                 <td><input type="checkbox" class="form-check-input" value="'.$row["id"].'"/></td>
+                           </tr>
+                      ';
+                    }
+                  ?>
+                </tbody>
+            </table>
+
+        </div>
+
+        <script src="../../static/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
